70be972c18b0e96add493487bfb9afa1d765a700
Mbrrkskn <keskinmuberra111@gmail.com>
2024-09-20 08:32:10 +0300

    Dosyalar eklendi

--- 1-DbFirst/Program.cs
+++ 1-DbFirst/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _1_DbFirst
+﻿using _1_DbFirst.Entities;
+
+namespace _1_DbFirst
 {
     internal class Program
     {
@@ -9,19 +11,41 @@
          * 
          *  dotnet tool install --global dotnet-ef komutu ef tool paketini kurar
          *  
-         *  Ayrica projemize EntityFrameWorkCore paketini ve hangi database ile calisacak isek 
+         *  Ayrica projemize hangi database ile calisacak isek 
          *  Mesela EntityFrameWorkCore.SqlServer ilgili paketini ve EntityFrameWorkCore.Design paketini kurmak gereklidir.
          *  Bu paketleri nuget uzerinden indirebiliriz
-         * 
-         * Halihazırdaki bir database'in DbContext'ini cikartmak için asagıdaki komut kullanılabilir
-         * Burada Connection string ve hangi database paketini kullanacağımız değiştirmek yeterli olacaktır.
-         * 
-         * 
+         *  
+         *  Halihazirdaki bir database'in DbContext'ini cikartmak icin aşagidaki komut kullanilabilir.
+         *  Burada Connection string ve Hangi database paketi ni kullanacagimiz degistirmek yeterli olacaktir.
+         *  
+         *  dotnet ef dbcontext scaffold 
+         *  "server=.;Database=Northwind;Trusted_Connection=true;TrustServerCertificate=true"  //Burasi Connection string bolumu
+         *  Microsoft.EntityFrameWorkCore.SqlServer    // Hangi databasea ile calisacagi bilgisi
+         *  -o Entities  // output olarak hangi Klasore cikti verecegi
          */
         #endregion
-
         static void Main(string[] args)
         {
+            Shipper shipper = new Shipper()
+            {
+                ShipperId = 11,
+                CompanyName = null,
+                Phone = "123123"
+            };
+            NorthwindContext context = new NorthwindContext();
+
+            // context.Customers.Where(p => p.Country == "UK").ToList().ForEach(p => Console.WriteLine(p.CompanyName + " " + p.Country));
+
+            //Update
+            //var shipper11 = context.Find<Shipper>(11);
+            //shipper11.CompanyName = "Deneme";
+            //context.Shippers.Update(shipper11);
+            //Insert
+            //context.Shippers.Add(shipper);
+
+            context.Shippers.Update(shipper);
+
+            context.SaveChanges();
             Console.WriteLine("Hello, World!");
         }
     }
